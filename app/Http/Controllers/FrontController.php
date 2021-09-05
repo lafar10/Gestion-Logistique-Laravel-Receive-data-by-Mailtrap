ec685e6d7466f3a04ed03fe3907994f9b1979ff6
lafar10 <ayoub_lafar@hotmail.com>
2021-09-05 17:57:44 +0100

    initial commit

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -39,7 +39,7 @@ class FrontController extends Controller
             'vehicule_type'
         ]);
 
-        Mail::to('ayoub7@gmail.com')
+        Mail::to('ayoublafar7@gmail.com')
             ->send(new CreateCommades($data));
 
         return redirect()->back()->with('flash', 'success');
