6e0d1ba2200ce31dab96b590e263b80164875c8f
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2021-10-05 15:55:54 +0100

    Update FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -19,9 +19,61 @@ class FrontController extends Controller
     {
         return view('Front.triporteur');
     }
-
-    public function triporteur_gmail()
+    
+     protected function messages()
     {
+        return $messages = [
+
+            'lieu_ramassage.required' => __('messages.lram'),
+            'lieu_depose.required' => __('messages.lds'),
+            'date_ramassage.required' => __('messages.dram'),
+            'heure.required' => __('messages.heure'),
+            'minutes.required' => __('messages.minutes'),
+            'period.required' => __('messages.period'),
+            'name.required' => __('messages.nom'),
+            'prenom.required' => __('messages.prenom'),
+            'email.required' => __('messages.email'),
+            'tel.required' => __('messages.tel'),
+            'tel.integer' => __('messages.tel integer'),
+            'tel.max' => __('messages.tel max'),
+            'tel.min' => __('messages.tel min'),
+            'adresse.required' => __('messages.adresse'),
+
+        ];
+    }
+
+    protected function rules()
+    {
+        return $rules = [
+            'lieu_ramassage' => 'required',
+            'lieu_depose' => 'required',
+            'date_ramassage' => 'required',
+            'heure' => 'required',
+            'minutes' => 'required',
+            'period' => 'required',
+            'name' => 'required|min:2|max:50',
+            'prenom' => 'required|min:2|max:50',
+            'email' => 'required|email',
+            'tel' => 'required|max:18',
+            'adresse' => 'required|max:255',
+
+        ];
+    }
+
+
+    public function triporteur_gmail(Request $request)
+    {
+        $rules = $this->rules();
+        $messages = $this->messages();
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput($request->all());
+        }
+        
         $data = request([
             'name',
             'prenom',
