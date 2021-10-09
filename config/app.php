12489e1ce402808ea24715d83bbc83c0a096b742
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2021-10-09 10:03:39 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://lr10-fayaj.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
