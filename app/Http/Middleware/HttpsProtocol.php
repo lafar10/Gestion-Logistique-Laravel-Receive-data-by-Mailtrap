d7e08378e274ff6d6e4d5863ccef62d4779e5801
lafar10 <ayoub_lafar@hotmail.com>
2021-10-09 11:14:18 +0100

    initial commit

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!$request->secure())
-        {
-            return redirect()->secure($request->getRequestUri());
-        }
+
         return $next($request);
     }
 }
