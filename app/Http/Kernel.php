c411ef32383869e45854e9d63f73c43006689b55
lafar10 <ayoub_lafar@hotmail.com>
2021-10-09 10:54:51 +0100

    initial commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Local::class,
+            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
@@ -64,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        
+
 
     ];
 }
